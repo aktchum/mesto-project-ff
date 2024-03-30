736cb872493af8d5f879dd900c198d4dbce9d47c
artem pravdin <arch@MacBook-Pro-Artem.local>
2024-03-30 23:02:41 +0400

    работа 5 на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
+const userTemplate = document.querySelector('#card-template').content;
+const usersOnline = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+initialCards.forEach(function (card) {
+  const userElement = userTemplate.querySelector('.places__item.card').cloneNode(true);
 
+  userElement.querySelector('.card__image').src = card.link;
+  userElement.querySelector('.card__image').alt = card.name;
+  userElement.querySelector('.card__title').textContent = card.name;
 // @todo: Функция удаления карточки
-
+  const deleteButton = userElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    userElement.remove()
+  });
 // @todo: Вывести карточки на страницу
+  usersOnline.append(userElement);
+});
