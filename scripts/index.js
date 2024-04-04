d9a619d9e7b27aaee67d91da85ed016324a0c900
artem pravdin <arch@MacBook-Pro-Artem.local>
2024-04-04 19:42:30 +0400

    добавлена функция коллбэк удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,37 @@
-// @todo: Темплейт карточки
-// @todo: DOM узлы
+// Получаем шаблон и контейнер для карточек
 const userTemplate = document.querySelector('#card-template').content;
 const usersOnline = document.querySelector('.places__list');
-// @todo: Функция создания карточки
-initialCards.forEach(function (card) {
+
+// Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
   const userElement = userTemplate.querySelector('.places__item.card').cloneNode(true);
 
-  userElement.querySelector('.card__image').src = card.link;
-  userElement.querySelector('.card__image').alt = card.name;
-  userElement.querySelector('.card__title').textContent = card.name;
-// @todo: Функция удаления карточки
+  userElement.querySelector('.card__image').src = cardData.link;
+  userElement.querySelector('.card__image').alt = cardData.name;
+  userElement.querySelector('.card__title').textContent = cardData.name;
+
+  // Устанавливаем функцию удаления карточки
   const deleteButton = userElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', function() {
-    userElement.remove()
+    deleteCardCallback(userElement);
   });
-// @todo: Вывести карточки на страницу
-  usersOnline.append(userElement);
+
+  return userElement;
+}
+
+// Функция-колбэк для удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Перебираем массив карточек и добавляем их на страницу
+initialCards.forEach(function (card) {
+  const cardElement = createCard(card, deleteCard);
+  usersOnline.append(cardElement);
 });
+
+
+
+
+
+
